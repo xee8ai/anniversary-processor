7eb6a1daeaeaf2015b7cbc849d0ab8c8ea347b7a
xee8ai <xee8ai@users.noreply.github.com>
2016-11-29 18:15:09 +0100

    Activate venv in pdf processor.

--- pdf-processor.sh
+++ pdf-processor.sh
@@ -2,4 +2,9 @@
 
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# If there is a local venv: activate
+if [ -d $BASE_DIR/venv ]; then
+	source $BASE_DIR/venv/bin/activate
+fi
+
 /usr/bin/env python3 $BASE_DIR/src/anniversary-processor.py pdf
